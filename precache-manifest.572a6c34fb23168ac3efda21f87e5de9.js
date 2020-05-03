4fcf31d90768c1fd65d7a9bb8d46bb7db0754ea0
debashish sahoo <debashish4@gmail.com>
2020-05-04 00:44:03 +0530

    initial commit

---
+++ precache-manifest.572a6c34fb23168ac3efda21f87e5de9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a74a16f322da8cbb9a2",
+    "url": "/fe/css/about.31f9384b.css"
+  },
+  {
+    "revision": "681c3079c30c7155252b",
+    "url": "/fe/css/app.1a3020af.css"
+  },
+  {
+    "revision": "c6d27946a650873e079f0e5668a2faf5",
+    "url": "/fe/img/hotel-wallpaper-black.c6d27946.png"
+  },
+  {
+    "revision": "4b8c6162c1cc91694d8b36feca42cc94",
+    "url": "/fe/img/hotel-wallpaper.4b8c6162.png"
+  },
+  {
+    "revision": "5e696d41139dd82469a3c952a22d27d7",
+    "url": "/fe/index.html"
+  },
+  {
+    "revision": "0a74a16f322da8cbb9a2",
+    "url": "/fe/js/about.bac94ca3.js"
+  },
+  {
+    "revision": "681c3079c30c7155252b",
+    "url": "/fe/js/app.49ee5791.js"
+  },
+  {
+    "revision": "9602edf1497b8e4a393a",
+    "url": "/fe/js/chunk-vendors.339175de.js"
+  },
+  {
+    "revision": "230337b6fe5c24a47015bffdeac6e23b",
+    "url": "/fe/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fe/robots.txt"
+  }
+]);
